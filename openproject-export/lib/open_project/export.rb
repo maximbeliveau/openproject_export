be486989e00e2251e4b11586a12f6f21acc2f61e
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-05 17:15:32 -0400

    Ensure export hooks load after dependencies

--- openproject-export/lib/open_project/export.rb
+++ openproject-export/lib/open_project/export.rb
@@ -1,5 +1,8 @@
 module OpenProject
   module Export
     require "open_project/export/engine"
+    require 'redmine/i18n'
+    require 'open_project/hook'
+    require "open_project/export/hooks"
   end
 end
