23329eb3d602891be3b405931d3ab253b64725bd
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-05 16:53:41 -0400

    Require Redmine i18n for hooks

--- openproject-export/lib/open_project/export/hooks.rb
+++ openproject-export/lib/open_project/export/hooks.rb
@@ -1,5 +1,7 @@
 module OpenProject
   module Export
+    require 'redmine/i18n'
+    require 'open_project/hook'
     class Hooks < OpenProject::Hook::ViewListener
       render_on :view_projects_settings_menu,
                 partial: 'open_project/export/hooks/download_all_button'
