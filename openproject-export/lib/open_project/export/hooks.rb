ec0b642ef73288e06053e5818f2f650159a4379b
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-06 15:43:39 -0400

    Update rails dependency for compatibility

--- openproject-export/lib/open_project/export/hooks.rb
+++ openproject-export/lib/open_project/export/hooks.rb
@@ -14,7 +14,8 @@ module OpenProject
     end
     class Hooks < OpenProject::Hook::ViewListener
       render_on :view_projects_settings_menu,
-                partial: 'open_project/export/hooks/download_all_button'
+                partial: "open_project/export/hooks/backup_button"
     end
   end
 end
+
