cfa771ea3d4665c4e17999886ffc5f2ce0474d0d
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-06 10:24:36 -0400

    Add Backup Project button

--- openproject-export/config/routes.rb
+++ openproject-export/config/routes.rb
@@ -2,4 +2,8 @@ OpenProject::Export::Engine.routes.draw do
   get '/projects/:project_id/attachments/download_all',
       to: 'attachments#download_all',
       as: 'download_all_project_attachments'
+  get "/projects/:project_id/backup",
+      to: "backups#download",
+      as: "backup_project"
 end
+
