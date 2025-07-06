4d954f89e4d49fef677e21303ce597d583b8f433
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-05 21:49:08 -0400

    Handle missing OpenProject environment

--- openproject-export/lib/open_project/export.rb
+++ openproject-export/lib/open_project/export.rb
@@ -1,6 +1,10 @@
 module OpenProject
   module Export
     require "open_project/export/engine"
-    require "open_project/export/hooks"
+    begin
+      require "open_project/export/hooks"
+    rescue LoadError
+      warn "OpenProject::Export::Hooks could not be loaded"
+    end
   end
 end
