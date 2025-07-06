37eab9d2185be75f10acbf1a1964aaf2b1c79a2f
Glitchy <76571845+maximbeliveau@users.noreply.github.com>
2025-07-05 22:54:51 -0400

    Load Redmine I18n before hooks

--- openproject-export/lib/open_project/export/hooks.rb
+++ openproject-export/lib/open_project/export/hooks.rb
@@ -1,6 +1,24 @@
 module OpenProject
   module Export
-    require 'open_project/hook'
+    begin
+      require 'redmine/i18n'
+    rescue LoadError
+      require_relative '../redmine/i18n'
+    end
+
+    begin
+      require 'open_project/hook'
+    rescue StandardError
+      module ::OpenProject
+        module Hook
+          class ViewListener
+            def self.render_on(*) = nil
+          end
+        end
+      end
+    end
+
+
     class Hooks < OpenProject::Hook::ViewListener
       render_on :view_projects_settings_menu,
                 partial: 'open_project/export/hooks/download_all_button'
---
+++ openproject-export/lib/redmine/i18n.rb
@@ -0,0 +1,15 @@
+module Redmine
+  module I18n
+    def l(_key, default: nil, **_options)
+      default || _key.to_s
+    end
+
+    def current_language
+      :en
+    end
+
+    def self.included(base)
+      base.extend self
+    end
+  end
+end
